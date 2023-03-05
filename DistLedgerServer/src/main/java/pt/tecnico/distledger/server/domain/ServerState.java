d469aa158c0b5ba1916bcd3ce56f34768bef6528
d-melita <dgmelita9@gmail.com>
2023-03-05 13:46:07 +0000

    feat: crated userAccount class and server boilerpart
    created class userAccount to manage user account in server side.
    this class already has all  constructors, getters, setters,
    toString and other important methods. In server, created attribute
    accounts which is a map and implemented constructors, setters and
    getters

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/ServerState.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/ServerState.java
@@ -1,18 +1,65 @@
 package pt.tecnico.distledger.server.domain;
 
 import pt.tecnico.distledger.server.domain.operation.Operation;
+import pt.tecnico.distledger.server.domain.userAccount;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 public class ServerState {
     private List<Operation> ledger;
 
+    Map<userAccount, Integer> accounts;
+
     public ServerState() {
         this.ledger = new ArrayList<>();
+        this.accounts = new HashMap<>();
     }
 
-    /* TODO: Here should be declared all the server state attributes
-         as well as the methods to access and interact with the state. */
+    public void addOperation(Operation op) {
+        this.ledger.add(op);
+    }
+
+    public void addAccount(userAccount account) {
+        this.accounts.put(account, account.getBalance());
+    }
+
+    public void updateAccount(userAccount account, Integer balance) {
+        this.accounts.put(account, balance); // put replaces the value if the key already exists
+    }
+
+    public Integer getAccountBalance(userAccount account) {
+        return this.accounts.get(account);
+    }
+
+    public List<Operation> getLedger() {
+        return this.ledger;
+    }
+
+    public Map<userAccount, Integer> getAccounts() {
+        return this.accounts;
+    }
+
+    public void printLedger() {
+        for (Operation op : this.ledger) {
+            System.out.println(op.toString());
+        }
+    }
+
+    public void printAccounts() {
+        for (Map.Entry<userAccount, Integer> entry : this.accounts.entrySet()) {
+            System.out.println(entry.getKey().toString() + " " + entry.getValue());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ServerState{" +
+                "ledger=" + ledger +
+                ", accounts=" + accounts +
+                '}';
+    }
 
 }
