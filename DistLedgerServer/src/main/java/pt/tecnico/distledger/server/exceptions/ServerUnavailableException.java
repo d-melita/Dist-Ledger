9755cb04b0efa8c07a801a6f9a1d885cd5e5149f
dcard <diogocardoso67@tecnico.ulisboa.pt>
2023-03-08 17:49:25 +0000

    Fix exception message

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/exceptions/ServerUnavailableException.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/exceptions/ServerUnavailableException.java
@@ -2,6 +2,6 @@ package pt.tecnico.distledger.server.exceptions;
 
 public class ServerUnavailableException extends RuntimeException {
     public ServerUnavailableException() {
-        super("Server is unavailable");
+        super("UNAVAILABLE");
     }
 }
