ce1d90aab1faa9cb5f966dd66abf2c61cb2f1ff0
Diogo Cardoso <diogocardoso67@tecnico.ulisboa.pt>
2023-03-10 14:36:30 +0000

    Add caller class to logger

--- Utils/src/main/java/pt/tecnico/distledger/utils/Logger.java
+++ Utils/src/main/java/pt/tecnico/distledger/utils/Logger.java
@@ -1,11 +1,13 @@
 package pt.tecnico.distledger.utils;
+import java.lang.reflect.*;
 
 public final class Logger {
     private static boolean active = (System.getProperty("debug") != null);
 
     public static void log(Object o) {
         if (active) {
-            System.err.println(o);
+            String className[] = Thread.currentThread().getStackTrace()[2].getClassName().split("[.]");
+            System.err.println("[" + className[className.length - 1].toUpperCase() + "]: " + o);
         }
     }
 }
