c746ba274a2d036965296eed5fefdf5c99841106
d-melita <dgmelita9@gmail.com>
2023-03-10 15:49:09 +0000

    added message of debug when server is unavailable

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/exceptions/ServerUnavailableException.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/exceptions/ServerUnavailableException.java
@@ -1,7 +1,10 @@
 package pt.tecnico.distledger.server.exceptions;
 
+import pt.tecnico.distledger.utils.Logger;
+
 public class ServerUnavailableException extends RuntimeException {
     public ServerUnavailableException() {
         super("UNAVAILABLE");
+        Logger.log("Server unavailable");
     }
 }
