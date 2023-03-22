727de3ce8a45ec22bba2c93d529c7a7f33522d61
d-melita <dgmelita9@gmail.com>
2023-03-22 14:25:12 +0000

    add cache for user

--- User/src/main/java/pt/tecnico/distledger/userclient/grpc/UserService.java
+++ User/src/main/java/pt/tecnico/distledger/userclient/grpc/UserService.java
@@ -7,81 +7,86 @@ import pt.ulisboa.tecnico.distledger.contract.namingserver.NamingServerServiceGr
 import pt.ulisboa.tecnico.distledger.contract.namingserver.NamingServerDistLedger.*;
 import pt.ulisboa.tecnico.distledger.contract.user.UserServiceGrpc;
 
-import pt.tecnico.distledger.utils.Logger;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserService implements AutoCloseable {
 
-    private final NamingServerServiceGrpc.NamingServerServiceBlockingStub stub;
-    private final ManagedChannel channel;
+    private final NamingServerServiceGrpc.NamingServerServiceBlockingStub namingServerStub;
+    private final ManagedChannel namingServerChannel;
     private final String service = "DistLedger";
-    private ManagedChannel sv_channel;
-    private UserServiceGrpc.UserServiceBlockingStub sv_stub;
+    private Map<String, ManagedChannel> serverChannels;
+    private Map<String, UserServiceGrpc.UserServiceBlockingStub> serverStubs;
 
     public UserService(String host, int port) {
-        this.channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
-        this.stub = NamingServerServiceGrpc.newBlockingStub(this.channel);
+        this.namingServerChannel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
+        this.namingServerStub = NamingServerServiceGrpc.newBlockingStub(this.namingServerChannel);
+        this.serverChannels = new HashMap<>();
+        this.serverStubs = new HashMap<>();
     }
 
-    private void connectToServer(String server) {
-        LookupResponse response = stub.lookup(LookupRequest.newBuilder().setService(service).setQualifier(server).build());
+    private void cacheStub(String server){
+        if (this.serverStubs.get(server) != null && this.serverChannels.get(server) != null)
+            return;
+        LookupResponse response = namingServerStub.lookup(LookupRequest.newBuilder().setService(service).build());
         if (response.getHostsCount() == 0)
             throw new RuntimeException("Server not found");
         String sv[] = response.getHosts(0).split(":");
         String host = sv[0];
         int port = Integer.parseInt(sv[1]);
-        this.sv_channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
-        this.sv_stub = UserServiceGrpc.newBlockingStub(sv_channel);
+        this.serverChannels.put(server, ManagedChannelBuilder.forAddress(host, port).usePlaintext().build());
+        this.serverStubs.put(server, UserServiceGrpc.newBlockingStub(serverChannels.get(server)));
+    }
+
+    private void invalidateAndCacheStub(String server){
+        this.serverChannels.remove(server);
+        this.serverStubs.remove(server);
+        cacheStub(server);
     }
 
     public void createAccount(String server, String username) {
         try {
-            connectToServer(server);
-        } catch (RuntimeException e) {
-            System.out.println("Server not found");
-            return;
+            cacheStub(server);
+            this.serverStubs.get(server).createAccount(CreateAccountRequest.newBuilder().setUserId(username).build());
+        } catch (Exception e) {
+            invalidateAndCacheStub(server);
+            this.serverStubs.get(server).createAccount(CreateAccountRequest.newBuilder().setUserId(username).build());
         }
-        CreateAccountRequest request = CreateAccountRequest.newBuilder().setUserId(username).build();
-        Logger.log("Request is: " + request.toString());
-        sv_stub.createAccount(request);
     }
 
     public void deleteAccount(String server, String username) {
         try {
-            connectToServer(server);
-        } catch (RuntimeException e) {
-            System.out.println("Server not found");
-            return;
+            cacheStub(server);
+            this.serverStubs.get(server).deleteAccount(DeleteAccountRequest.newBuilder().setUserId(username).build());
+        } catch (Exception e) {
+            invalidateAndCacheStub(server);
+            this.serverStubs.get(server).deleteAccount(DeleteAccountRequest.newBuilder().setUserId(username).build());
         }
-        DeleteAccountRequest request = DeleteAccountRequest.newBuilder().setUserId(username).build();
-        Logger.log("Request is: " + request.toString());
-        sv_stub.deleteAccount(request);
     }
 
     public BalanceResponse balance(String server, String username) {
         try {
-            connectToServer(server);
-        } catch (RuntimeException e) {
-            System.out.println("Server not found");
-            return null;
+            cacheStub(server);
+            return this.serverStubs.get(server).balance(BalanceRequest.newBuilder().setUserId(username).build());
+        } catch (Exception e) {
+            invalidateAndCacheStub(server);
+            return this.serverStubs.get(server).balance(BalanceRequest.newBuilder().setUserId(username).build());
         }
-        BalanceRequest request = BalanceRequest.newBuilder().setUserId(username).build();
-        Logger.log("Request is: " + request.toString());
-        return sv_stub.balance(request);
     }
 
     public void transferTo(String server, String from, String dest, int amount) {
         try {
-            connectToServer(server);
-        } catch (RuntimeException e) {
-            System.out.println("Server not found");
-            return;
+            cacheStub(server);
+            this.serverStubs.get(server).transferTo(TransferToRequest.newBuilder().setAccountFrom(from).setAccountTo(dest).setAmount(amount).build());
+        } catch (Exception e) {
+            invalidateAndCacheStub(server);
+            this.serverStubs.get(server).transferTo(TransferToRequest.newBuilder().setAccountFrom(from).setAccountTo(dest).setAmount(amount).build());
         }
-        sv_stub.transferTo(TransferToRequest.newBuilder().setAccountFrom(from).setAccountTo(dest).setAmount(amount).build());
     }
 
     @Override
     public void close() {
-        this.channel.shutdownNow();
-        this.sv_channel.shutdownNow();
+        this.namingServerChannel.shutdownNow();
+        this.serverChannels.forEach((k, v) -> v.shutdownNow());
     }
 }
